b30be21f698282a3fe57511413d3277dd0f68f1d
vaibhav666-glitch <vaibhavning@gmail.com>
2023-07-06 00:53:40 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
 
-console.log(txtInput);
+
 btnTranslate.addEventListener("click",function clickEventHandler() {
-    console.log("clicked")
-    console.log("input", txtInput.value);
+ outputDiv.innerText ="ajajajasfsdfekjsd " + txtInput.value;
 
 })

621d4c9ae3d0866ebe2987c93c8b289c0773c32d
vaibhav666-glitch <vaibhavning@gmail.com>
2023-07-08 12:28:46 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
 
 
-btnTranslate.addEventListener("click",function clickEventHandler() {
- outputDiv.innerText ="ajajajasfsdfekjsd " + txtInput.value;
+function getTranslationUrl(text){
+    return serverUrl + "?" + "text=" + text
+}
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time");
+}
+function clichHandler(){
+    var inputText=txtInput.value;// taking input
+     //calling server for processing
+     fetch(getTranslationUrl(inputText))
+     .then(response=>response.json())
+     .then(json=>{outputDiv.innerText=json.contents.translated;})// output
+     .catch(errorHandler)
+};
 
-})
+btnTranslate.addEventListener("click", clichHandler)
